96f9f8503056a183f324b37a2acf311eaa4d6e73
mayur2410-tech <mayurwaykar9@gmail.com>
2025-12-13 10:26:06 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                sum+=nums[i]+nums[i+1];
-                i++;
-            }
+        int result =0;
+        for(int i = 0 ; i < nums.size(); i++){
+            result = result ^ nums[i];
         }
-        return accumulate(nums.begin(),nums.end(),0)-sum;
+        return result;
+
     }
 };
