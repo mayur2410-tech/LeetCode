443c13656c25337f384db2ac66773915a7a20c57
mayur2410-tech <mayurwaykar9@gmail.com>
2025-12-31 20:14:02 +0530

    Time: 0 ms (100%), Space: 9.3 MB (78.78%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        //brute force approach
+        // if(s.length()!=t.length()) return false;
+        // unordered_map<char,char>m1;
+        // unordered_map<char,char>m2;
+        // for(int i =0 ; i <s.length();i++ ){
+        //     if(m1.find(s[i])!=m1.end()){
+        //         if(m1[s[i]]!=t[i]){
+        //             return false;
+        //         }
+        //     }else{
+        //         m1[s[i]]= t[i];
+        //     }
+
+        //     if(m2.find(t[i])!=m2.end()){
+        //         if(m2[t[i]]!=s[i]){
+        //             return false;
+        //         }
+        //     }else{
+        //         m2[t[i]]= s[i];
+        //     }
+        // }
+        // return true;
+
+
+        //optimise approach
         if(s.size()!=t.size()) return false;
 
         int m1[256] ={0};
