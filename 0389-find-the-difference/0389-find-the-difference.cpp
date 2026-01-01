7b95c70d5a8eebdf73f325e1e340747e8b1a2cd6
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-01 18:14:11 +0530

    Time: 0 ms (100%), Space: 9.4 MB (22.78%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        //aproach 1 = using xor
-        char ans  =0 ;
-        for(int i = 0 ; i <s.length();i++){
-            ans ^= s[i];
-        }
-        for(int i = 0 ; i <t.length();i++){
-            ans ^= t[i];
-        }
+        // aproach 1 = using xor
+        //  char ans  =0 ;
+        //  for(int i = 0 ; i <s.length();i++){
+        //      ans ^= s[i];
+        //  }
+        //  for(int i = 0 ; i <t.length();i++){
+        //      ans ^= t[i];
+        //  }
 
-        return ans;
+        // return ans;
 
+        // appraoch 2 = using map
+        unordered_map<char, int> m1;
+        char ans;
+        for (int i = 0; i < s.size(); i++) {
+            m1[s[i]]++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            if(m1[t[i]]==0){
+                return t[i];
+            }else{
+                m1[t[i]]--;
+            }
+           
+        }
+        return ' ';
     }
 };
