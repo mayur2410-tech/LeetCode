0e3500bc5d7fe0bf22c5d9a6bfbd92ce12646bb3
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-03 10:34:01 +0530

    Time: 8 ms (50.58%), Space: 128.3 MB (56.34%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        // apprach 1 = using 2 list we store positive and negeative then push it again to new ans
-        vector<int>pos;
-        vector<int>neg;
+        // apprach 1 = using 2 list we store positive and negeative then push it again to new ans 
+        // Complexity = time [O(n)] and space [O(n)]
+        // vector<int>pos;
+        // vector<int>neg;
+        // for(int i =0 ; i <nums.size();i++){
+        //     if(nums[i]>0){
+        //         pos.push_back(nums[i]);
+        //     }else{
+        //         neg.push_back(nums[i]);
+        //     }
+        // }
+        // vector<int> ans;
+        // int i=0;
+        // while(i<pos.size()){
+        //     ans.push_back(pos[i]);
+        //     ans.push_back(neg[i]);
+        //     i++;
+        // }
+
+        // return ans;
+
+        // appraoch 2
+        int pos = 0 , neg = 1;
+        vector<int>ans(nums.size(),0);
         for(int i =0 ; i <nums.size();i++){
             if(nums[i]>0){
-                pos.push_back(nums[i]);
+                ans[pos]=nums[i];
+                pos+=2;
             }else{
-                neg.push_back(nums[i]);
+                ans[neg]=nums[i];
+                neg+=2;
             }
-        }
-        vector<int> ans;
-        int i=0;
-        while(i<pos.size()){
-            ans.push_back(pos[i]);
-            ans.push_back(neg[i]);
-            i++;
         }
 
         return ans;
