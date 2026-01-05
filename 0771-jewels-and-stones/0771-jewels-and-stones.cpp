06d457600d255d3b43208c7ba83144f18b7e401d
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-05 12:04:40 +0530

    Time: 0 ms (100%), Space: 8.9 MB (6.85%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
        //brute force
-       int count=0;
-       for(int i =0 ; i <jewels.size();i++){
-        for(int j =0; j <stones.size();j++){
-            if(jewels[i]==stones[j]){
+    //    int count=0;
+    //    for(int i =0 ; i <jewels.size();i++){
+    //     for(int j =0; j <stones.size();j++){
+    //         if(jewels[i]==stones[j]){
+    //             count++;
+    //         }
+    //     }
+    //    }
+    //    return count;
+
+    //ptimize approach
+        unordered_set<char> s;
+        int count=0;
+        for(int i=0; i <jewels.size();i++){
+            s.insert(jewels[i]);
+        }
+        for(int i=0; i <stones.size();i++){
+            if(s.find(stones[i])!=s.end()){
                 count++;
             }
         }
-       }
-       return count;
+        return count;
     }
 };
