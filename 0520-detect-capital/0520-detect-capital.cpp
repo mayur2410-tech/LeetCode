fc0d1ccb24499f834093063500cfb5c068f4a326
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-05 12:18:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.37%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size()==1 && (word[0]>='A'&& word[0]<='Z')){
+            return true;
+        }
+        int count=0;
+        for(int i=0 ; i<word.size();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                count++;
+            }
+        }
+
+        if(count==word.length()){
+            return true;
+        }else if(count==0){
+            return true;
+        }else if (count==1 && word[0]>='A' && word[0]<='Z'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
