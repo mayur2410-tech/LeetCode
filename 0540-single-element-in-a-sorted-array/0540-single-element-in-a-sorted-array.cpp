626602be98566496b6e4f85f50814b2bf61fe15b
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-06 14:37:57 +0530

    Time: 0 ms (100%), Space: 26.1 MB (67.01%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         // apporach 1 = O(n) 
-if(nums.size()==1){
-    return nums[0];
-}
+// if(nums.size()==1){
+//     return nums[0];
+// }
 
-        for(int i = 0 ; i <nums.size()-2;i=i+2){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
-                break;
-            }
+//         for(int i = 0 ; i <nums.size()-2;i=i+2){
+//             if(nums[i]!=nums[i+1]){
+//                 return nums[i];
+//                 break;
+//             }
            
-        }
+//         }
 
-        return nums[nums.size()-1];
+//         return nums[nums.size()-1];
+
+// appraoch 2 = using binary search
+ 
+ int st =0 ;
+ int end = nums.size()-1;
+ while(st<end){
+    int mid = st + (end-st)/2;
+    if(mid%2==1){
+        mid--;
+    }
+    if(nums[mid]==nums[mid+1]){
+        st = mid+2;
+    }else{
+        end = mid;
+    }
+ }
+ return nums[st];
+
     }
 };
