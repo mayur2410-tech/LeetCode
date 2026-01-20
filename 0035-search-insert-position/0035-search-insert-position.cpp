db24f2dd9d85391fcf3240565f8ecce0c82b661f
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-20 10:00:27 +0530

    Time: 0 ms (100%), Space: 13.7 MB (41.23%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for(int i =0 ; i<nums.size();i++){
-            if(nums[i]>=target){
-                return i;
+        // apprach 1 = 
+        // for(int i =0 ; i<nums.size();i++){
+        //     if(nums[i]>=target){
+        //         return i;
+        //     }
+        // }
+        // return nums.size();
+
+        // approach 2
+      
+        int st = 0 ;
+        int end = nums.size()-1;
+        int ans = nums.size();
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(nums[mid]>=target){
+                ans =  mid;
+                end = mid -1;
+            }else{
+                st = mid+1;
             }
         }
-        return nums.size();
+
+        return ans;
     }
 };
