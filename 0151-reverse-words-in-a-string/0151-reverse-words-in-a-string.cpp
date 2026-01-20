c5c8c62ef772fe84e19bc429432628bd123f0712
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-20 12:31:00 +0530

    Time: 3 ms (37.16%), Space: 10.3 MB (64.76%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string temp= "";
-        string ans="";
-        for(int i = s.size()-1; i>=0;i--){
-            if(s[i] != ' '){
-                temp += s[i];
-            }else{
-                if(!temp.empty()){
-                    reverse(temp.begin(),temp.end());
-                    ans += temp + " ";
-                    temp.clear();
-                }
+        //apracoh 1
+        // string temp= "";
+        // string ans="";
+        // for(int i = s.size()-1; i>=0;i--){
+        //     if(s[i] != ' '){
+        //         temp += s[i];
+        //     }else{
+        //         if(!temp.empty()){
+        //             reverse(temp.begin(),temp.end());
+        //             ans += temp + " ";
+        //             temp.clear();
+        //         }
+        //     }
+        // }
+
+        // if(!temp.empty()){
+        //     reverse(temp.begin(),temp.end());
+        //     ans += temp;
+        // }else if(!ans.empty()){
+        //     ans.pop_back();
+        // }
+
+        // return ans;
+
+        // approach 2 = 
+        string ans = "";
+        reverse(s.begin(),s.end());
+        for(int i = 0; i <s.size();i++){
+                    string word = "";
+
+            while( i < s.size() && s[i]!= ' '){
+                word+= s[i];
+                i++;
+            }
+            reverse(word.begin(),word.end());
+            if(word.size()>0){
+                ans += " "+ word;
             }
         }
-
-        if(!temp.empty()){
-            reverse(temp.begin(),temp.end());
-            ans += temp;
-        }else if(!ans.empty()){
-            ans.pop_back();
-        }
-
-        return ans;
+        return ans.substr(1);
     }
 };
