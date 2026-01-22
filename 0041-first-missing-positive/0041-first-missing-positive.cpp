ebb41d4bcde0375662f576b2bf9f1c8cc4074b46
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-22 16:03:04 +0530

    Time: 0 ms (100%), Space: 55.1 MB (32.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set <int> s;
-        for(int i=0; i <nums.size();i++){
-            s.insert(nums[i]);
-        }
-            int target = 1;
-        while(true){
-                if(s.find(target)==s.end()){
-                    return target;
-                }
+        // appraoch 1 = it take  o(n)
+        // unordered_set <int> s;
+        // for(int i=0; i <nums.size();i++){
+        //     s.insert(nums[i]);
+        // }
+        //     int target = 1;
+        // while(true){
+        //         if(s.find(target)==s.end()){
+        //             return target;
+        //         }
                
-                    target++;
+        //             target++;
                 
+        // }
+        // return 0;
+        // appporach 2 = using cyclic sort
+        int n = nums.size();
+        int i =  0 ; 
+        while(i < n){
+            if(nums[i]>=1 && nums[i]<=n ){
+                int correctIdx = nums[i]-1;
+                if(nums[i]!=nums[correctIdx]){
+                    swap(nums[i],nums[correctIdx]);
+                    continue;
+                }
+            }
+            i++;
         }
-        return 0;
+     for(int i =0; i <n ; i++){
+        if(nums[i]!= i+1){
+            return i+1;
+        }
+     }
+
+     return n+1;
     }
 };
