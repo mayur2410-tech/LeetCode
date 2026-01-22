cb5f4a82c60090408807238080b248ecd080bfa5
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-22 11:56:25 +0530

    Time: 0 ms (100%), Space: 9.3 MB (22.24%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-       size_t pos = haystack.find(needle);
-       if(pos!= string::npos){
-        return pos;
-       }
-       return -1;
+        // appraoch 1 = with function 
+    //    size_t pos = haystack.find(needle);
+    //    if(pos!= string::npos){
+    //     return pos;
+    //    }
+    //    return -1;
+
+    // approach 2 = manula find using substr
+
+    int m = haystack.size();
+    int n = needle.size();
+    for(int i = 0 ; i <= m - n ;i++){
+        if(haystack.substr(i,n)== needle){
+            return i;
+        }
+    }
+    return -1;
     }
 };
