487488094b32b028f2df40580b753d927937ba3c
mayur2410-tech <mayurwaykar9@gmail.com>
2026-01-30 10:54:23 +0530

    Time: 0 ms (100%), Space: 63.6 MB (46.65%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int max = arr[0];
+        int idx = 0 ;
+        for(int i = 0 ; i <arr.size();i++){
+            if(arr[i] > max){
+                max = arr[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+};
