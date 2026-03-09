a01d924fdb41d5ebf44a3dd94307554efd4e2ad3
mayur2410-tech <mayurwaykar9@gmail.com>
2026-03-09 09:59:58 +0530

    Time: 0 ms (100%), Space: 13.6 MB (76.44%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -10,20 +10,19 @@ public:
         // return nums.size();
 
         // approach 2
-      
-        int st = 0 ;
+    
+        int st =0 ;
         int end = nums.size()-1;
         int ans = nums.size();
         while(st<=end){
             int mid = st + (end-st)/2;
             if(nums[mid]>=target){
-                ans =  mid;
+                ans = mid;
                 end = mid -1;
             }else{
-                st = mid+1;
+                st=mid+1;
             }
         }
-
         return ans;
     }
 };
