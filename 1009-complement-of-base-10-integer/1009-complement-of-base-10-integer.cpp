e3991649888c7b85ffdfa7bd134576db200752ef
mayur2410-tech <mayurwaykar9@gmail.com>
2026-03-11 10:50:33 +0530

    Time: 0 ms (100%), Space: 7.8 MB (59.24%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -4,10 +4,23 @@ public:
         if(n==0){
             return 1;
         }
-        int mask=1;
-        while(mask<=n){
-            mask<<=1;
+        // int mask=1;
+        // while(mask<=n){
+        //     mask<<=1;
+        // }
+        // return mask-1-n;
+
+        //homsavi approach
+
+        int count =0;
+        int result = 0;
+        while(n){
+            int rem = n%2;
+            result += pow(2,count) * (!rem);
+            n=n/2;
+        count++;
+
         }
-        return mask-1-n;
+        return result;
     }
 };
