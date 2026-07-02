47cb099815adfd9800744ac5a5a2d495f9d4263d
mayur2410-tech <mayurwaykar9@gmail.com>
2026-07-02 07:34:05 +0530

    Time: 0 ms (100%), Space: 23.7 MB (42.21%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,25 +11,54 @@
  */
 class Solution {
 public:
+// approach 1 
+// int Height(TreeNode* root){
+//      if(root==nullptr){
+//             return 0;
+//         }
 
-int Height(TreeNode* root){
-     if(root==nullptr){
-            return 0;
-        }
+//         int leftHt = Height(root->left);
+//         int rightHt = Height(root->right);
 
-        int leftHt = Height(root->left);
-        int rightHt = Height(root->right);
+//         return max(rightHt,leftHt)+1;
+// }
 
-        return max(rightHt,leftHt)+1;
-}
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(root==nullptr){
-            return 0;
-        }
-        int currentDiameter = Height(root->left) + Height(root->right) ;
-        int leftDiameter =  diameterOfBinaryTree(root->left);
-        int rightDiameter =  diameterOfBinaryTree(root->right);
 
-        return max(currentDiameter,max(leftDiameter,rightDiameter));
+// approach 2
+ pair<int,int> Diameter(TreeNode* root){
+
+
+    if(root == nullptr){
+        return make_pair(0,0);
     }
+
+    pair<int,int> leftInfo = Diameter(root->left); //LD ,LH
+    pair<int,int> rightInfo = Diameter(root->right); //RD ,RH
+
+    int currentDia = leftInfo.second + rightInfo.second ;
+    int finalDia = max(currentDia,max(leftInfo.first,rightInfo.first));
+    int finalHt = max(leftInfo.second ,rightInfo.second)+1;
+    return make_pair(finalDia,finalHt);
+ 
+        }; 
+
+        int diameterOfBinaryTree(TreeNode* root) {
+//         //approach 1 = o(n^2)
+//         if(root==nullptr){
+//             return 0;
+//         }
+//         int currentDiameter = Height(root->left) + Height(root->right) ;
+//         int leftDiameter =  diameterOfBinaryTree(root->left);
+//         int rightDiameter =  diameterOfBinaryTree(root->right);
+
+//         return max(currentDiameter,max(leftDiameter,rightDiameter));
+//     
+
+ 
+ 
+
+
+        return Diameter(root).first;
+
+        }
 };
